219eb44c73d3746b378acbbe5d4bcee8afd7531c
EmaCst <dcastellanosv2@miumg.edu.gt>
2026-02-16 15:30:05 -0600

    3 commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
-import { generarDiagnostico } from "./controllers/diagnostico.controller.js"
+import diagnosticoRoutes from "./app/routes/diagnostico.routes.js"
 
 dotenv.config()
 
@@ -11,13 +11,13 @@ app.use(cors())
 app.use(express.json())
 
 app.get("/", (req, res) => {
-  res.send("API funcionando")
+  res.send("API funcionando 🚀")
 })
 
-app.post("/api/diagnostico", generarDiagnostico)
+app.use("/api/diagnostico", diagnosticoRoutes)
 
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log(`Servidor en puerto ${PORT}`) 
+  console.log(`Servidor en puerto ${PORT}`)
 })
